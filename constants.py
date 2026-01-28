53da000a491e4477952ff1f48054a38e3a2e4d89
Matvey26 <matvey.ivchenko@yandex.ru>
2026-01-28 23:23:31 +0300

    Add ci (linter), Refactor code

--- constants.py
+++ constants.py
@@ -1,5 +1,10 @@
 import os
 
+from dotenv import load_dotenv
+
+# Загружает переменные из .env в окружение
+load_dotenv()
+
 # get token using BotFather
 TOKEN = os.getenv("TG_TOKEN")
 
@@ -12,6 +17,6 @@ ZERO: str = "O"
 HELP_TURN_MESSAGE = "{sign} (your) turn! Please, put {sign} to the free place"
 END_MESSAGE = "The {sign} won!"
 
-HEIGHT, WIDTH = 3, 3
+WIDTH = 3
 
-DEFAULT_STATE = [ [FREE_SPACE for _ in range(WIDTH) ] for _ in range(HEIGHT) ]
+DEFAULT_STATE = [ [FREE_SPACE for _ in range(WIDTH) ] for _ in range(WIDTH) ]
--- main.py
+++ main.py
@@ -1,35 +1,22 @@
 #!/usr/bin/env python3
 
-"""
-Bot for playing tic tac toe game with multiple CallbackQueryHandlers.
-"""
+"""Bot for playing tic tac toe game with multiple CallbackQueryHandlers."""
 import logging
-import random
-from copy import deepcopy
-from config import (
-    TOKEN,
-    CONTINUE_GAME, FINISH_GAME,
-    FREE_SPACE, CROSS, ZERO,
-    HELP_TURN_MESSAGE, END_MESSAGE,
-    DEFAULT_STATE,
-    HEIGHT, WIDTH,
-)
-from dotenv import load_dotenv
-from functools import wraps
-from typing import Literal
 
-from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
+from telegram import Update
 from telegram.ext import (
     Application,
     CallbackQueryHandler,
     CommandHandler,
-    ContextTypes,
     ConversationHandler,
 )
 
-
-# Загружает переменные из .env в окружение
-load_dotenv()
+from bot_handlers import end, game, start
+from constants import (
+    CONTINUE_GAME,
+    FINISH_GAME,
+    TOKEN,
+)
 
 # Enable logging
 logging.basicConfig(
@@ -40,159 +27,6 @@ logging.getLogger("httpx").setLevel(logging.WARNING)
 
 logger = logging.getLogger(__name__)
 
-
-def get_default_state():
-    """Helper function to get default state of the game"""
-    return deepcopy(DEFAULT_STATE)
-
-
-def generate_keyboard(state: list[list[str]]) -> list[list[InlineKeyboardButton]]:
-    """Generate tic tac toe keyboard 3x3 (telegram buttons)"""
-    return [
-        [
-            InlineKeyboardButton(state[r][c], callback_data=f"{r}{c}")
-            for c in range(WIDTH)
-        ]
-        for r in range(HEIGHT)
-    ]
-
-
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Send message on `/start`."""
-    context.user_data["keyboard_state"] = get_default_state()
-    keyboard = generate_keyboard(context.user_data["keyboard_state"])
-    reply_markup = InlineKeyboardMarkup(keyboard)
-    await update.message.reply_text(HELP_TURN_MESSAGE.format(sign=CROSS),
-                                    reply_markup=reply_markup)
-    return CONTINUE_GAME
-
-
-def update_keyboard(func):
-    @wraps(func)
-    async def wrapper(update: Update, context: ContextTypes.DEFAULT_TYPE):
-        result = await func(update, context)
-
-        query = update.callback_query
-        fields = context.user_data["keyboard_state"]
-
-        # Обновляем сообщение-подсказку
-        if result == CONTINUE_GAME:
-            await query.edit_message_text(text=HELP_TURN_MESSAGE.format(sign=CROSS))
-        elif result == FINISH_GAME:
-            await query.edit_message_text(text=END_MESSAGE.format(sign=won(fields)))
-
-        # Обновляем клавиатуру
-        keyboard = generate_keyboard(fields)
-        reply_markup = InlineKeyboardMarkup(keyboard)
-        await query.edit_message_reply_markup(reply_markup=reply_markup)
-
-        return result
-
-    return wrapper
-
-
-@update_keyboard
-async def game(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Main processing of the game"""
-    fields = context.user_data["keyboard_state"]
-
-    # Получаем ход первого игрока (CROSS)
-    query = update.callback_query
-    callback_data = query.data
-
-    row, col = map(int, callback_data)
-
-    # Если первый игрок делает некорректный ход, то продолжаем игру
-    if not make_move(fields, row, col, CROSS):
-        return CONTINUE_GAME
-
-    # Если первый игрок делает победный ход, то завершаем игру
-    if won(fields):
-        return FINISH_GAME
-
-    # Получаем ход второго игрока (ZERO)
-    free_fields = [
-        (r, c)
-        for r in range(HEIGHT)
-        for c in range(WIDTH)
-        if fields[r][c] == FREE_SPACE
-    ]
-
-    row, col = random.choice(free_fields)
-
-    # Второй игрок всегда делает корректный ход
-    make_move(fields, row, col, ZERO)
-
-    # Если второй игрок делает победный ход, то завершаем игру
-    if won(fields):
-        return FINISH_GAME
-
-    return CONTINUE_GAME
-
-
-def make_move(
-    fields: list[str],
-    row: int,
-    column: int,
-    sign: Literal[CROSS, ZERO]
-) -> bool:
-    # Проверяем корректность хода
-    if not (0 <= row < HEIGHT and 0 <= column < WIDTH):
-        return False
-    if fields[row][column] != FREE_SPACE:
-        return False
-
-    # Делаем ход
-    fields[row][column] = sign
-    return True
-
-
-def won(fields: list[str]) -> Literal[CROSS, ZERO] | None:
-    """Check if crosses or zeros have won the game"""
-    for sign in [CROSS, ZERO]:
-        won_state = [sign] * 3
-        # Проверяем строки
-        for row_index in range(3):
-            row = fields[row_index]
-            if row == won_state:
-                return sign
-
-        # Проверяем столбцы
-        for col_index in range(3):
-            col = [
-                fields[row_index][col_index]
-                for row_index in range(3)
-            ]
-            if col == won_state:
-                return sign
-
-        # Проверяем главную диагональ
-        diag = [
-            fields[i][i]
-            for i in range(3)
-        ]
-        if diag == won_state:
-            return sign
-
-        # Проверяем побочную диагональ
-        side_diag = [
-            fields[i][2 - i]
-            for i in range(3)
-        ]
-        if side_diag == won_state:
-            return sign
-
-    return None
-
-
-async def end(update: Update, context: ContextTypes.DEFAULT_TYPE) -> int:
-    """Return `ConversationHandler.END`, which tells the
-    ConversationHandler that the conversation is over.
-    """
-
-    # reset state to default so you can play again with /start
-    context.user_data["keyboard_state"] = get_default_state()
-    return ConversationHandler.END
 
 
 def main() -> None:
